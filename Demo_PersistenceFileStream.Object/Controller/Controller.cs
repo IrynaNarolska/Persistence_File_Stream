616c4c543c8a9ee2cef50f0266a68fd8645eea6c
nothirdmolars - pippinj <pippinj@nmc.edu>
2016-10-14 09:36:01 -0400

    Setup controller with main game loop

--- Demo_PersistenceFileStream.Object/Controller/Controller.cs
+++ Demo_PersistenceFileStream.Object/Controller/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo_PersistenceFileStream.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,79 @@ using System.Threading.Tasks;
 
 namespace Demo_PersistenceFileStream.Controller
 {
-    class Controller
+    class AppController
     {
+        #region FIELDS
 
+        // instantiate the game view
+        private static ConsoleView _gameView = new ConsoleView();
+
+        // track app use
+        private bool _usingApp;
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public AppController()
+        {
+            InitializeApp();
+            UseApp();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// sets up app on initialization
+        /// </summary>
+        public void InitializeApp()
+        {
+            _usingApp = true;
+        }
+
+        /// <summary>
+        /// The main app loop
+        /// </summary>
+        public void UseApp()
+        {
+            while (_usingApp)
+            {
+                ManageStateTasks();
+            }
+        }
+
+        /// <summary>
+        /// designates app tasks based on view state
+        /// </summary>
+        public void ManageStateTasks()
+        {
+            switch (_gameView.CurrentViewState)
+            {
+                case ConsoleView.ViewState.WelcomeScreen:
+                    break;
+                case ConsoleView.ViewState.MainMenu:
+                    break;
+                case ConsoleView.ViewState.DisplayAllRecords:
+                    break;
+                case ConsoleView.ViewState.ClearAllRecords:
+                    break;
+                case ConsoleView.ViewState.AddRecord:
+                    break;
+                case ConsoleView.ViewState.DeleteRecord:
+                    break;
+                case ConsoleView.ViewState.UpdateRecord:
+                    break;
+                case ConsoleView.ViewState.Quit:
+                    _usingApp = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
     }
 
 }
